424d55af60410d5cc735357287ca5b38560a6992
Marie Foxall <marie@wastedeffort.ca>
2020-07-06 19:34:32 -0400

    Question 3

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require("morgan");
 
 const { top50 } = require("./data/top50");
 const topArtist = require("./data/topartist");
+const { books } = require("./data/books");
 
 const PORT = process.env.PORT || 8000;
 
@@ -16,6 +17,50 @@ app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 
 // endpoints here
+app.get("/books", (req, res) => {
+  res.status(200);
+  res.render("pages/booklist", {
+    title: "25 Books You Probably Should Have Read Already",
+    books: books,
+  });
+});
+
+app.get("/books/:id", (req, res) => {
+  const bookID = Number(req.params.id);
+  if (bookID > 100 && bookID < 126) {
+    res.status(200);
+    res.render("pages/uniquebook", {
+      title: `Book #${bookID}`,
+      bookID: bookID,
+      bookTitle: books[bookID - 101].title,
+      bookType: books[bookID - 101].type,
+      bookImgUrl: books[bookID - 101].imgUrl,
+      bookAuthor: books[bookID - 101].author,
+      bookDescription: books[bookID - 101].description,
+    });
+  }
+});
+
+app.get("/books/:type", (req, res) => {
+  const bookType = req.params.type;
+  const bookTypeArray = books.filter((element) => {
+    return element.type === bookType;
+  });
+  if (bookTypeArray !== undefined) {
+    console.log(bookTypeArray);
+    res.status(200);
+    res.render("pages/uniquebooktype", {
+      title: `Type: ${bookType}`,
+      bookTypeArray: bookTypeArray,
+    });
+  } else {
+    res.status(404);
+    res.render("pages/fourOhFour", {
+      title: "I got nothing",
+      path: req.originalUrl,
+    });
+  }
+});
 
 // handle 404s
 app.get("/top50", (req, res) => {
